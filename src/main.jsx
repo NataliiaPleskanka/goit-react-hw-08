7381acf6b5a5520c3ffcc165298cee53baebd050
NataliiaPleskanka <154823178+NataliiaPleskanka@users.noreply.github.com>
2024-10-29 13:13:30 +0100

    LayoutPatern

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store.js";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </BrowserRouter>
 );
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Phone Book</h1>
+    </div>
+  );
+};
+
+export default HomePage;
